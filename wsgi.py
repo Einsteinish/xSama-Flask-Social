1275cec20a4587a23f97c543384f482ac07e5c67
Ki Hong <sfvue@aol.com>
2017-01-14 03:31:50 -0800

    removed wsgi.sh and rename wsgi.py wsgi-xSama.py

--- wsgi.py
+++
@@ -1,8 +0,0 @@
-
-import os
-
-from app import app
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5550))
-    app.run(host='127.0.0.1', port=port)
--- wsgi.sh
+++
@@ -1,2 +0,0 @@
-source /home/k/MySites/xSama/venv/bin/activate
-/home/sfvue/MySites/xSama/venv/bin/python /home/sfvue/MySites/xSama/wsgi.py &
